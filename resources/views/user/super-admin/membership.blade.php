6fd3000534e3018b5a5fa53f171d647be1c6cbec
atalamahardika <atalamahardika24@gmail.com>
2025-06-17 12:00:12 +0700

    Membuat template view untuk CMS Membership

--- resources/views/user/super-admin/membership.blade.php
+++ resources/views/user/super-admin/membership.blade.php
@@ -8,9 +8,15 @@
             @includeWhen($title === 'Keanggotaan', 'components.content.main')
 
             {{-- Tulis kode di bawah ini untuk isi kontennya --}}
+            
         </x-layout.content-bar>
     </div>
 
     {{-- SweetAlert2 --}}
-    
+    @if (session('success'))
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-2 rounded mb-4">
+            {{ session('success') }}
+        </div>
+    @endif
+
 </body>
