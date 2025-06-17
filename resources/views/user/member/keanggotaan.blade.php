7d3f0ca2cc4658d1c7aecbcc2e3a9856ae64b0f8
atalamahardika <atalamahardika24@gmail.com>
2025-06-17 12:02:08 +0700

    Membuat template view tampilan keanggotaan di user member

---
+++ resources/views/user/member/keanggotaan.blade.php
@@ -0,0 +1,27 @@
+@include('head')
+
+<body>
+    <div class="flex h-screen">
+        <x-layout.sidebar />
+        <x-layout.content-bar :title="$title" :subtitle="$subtitle">
+            {{-- Slot utama di sini, bisa diganti sesuai konten --}}
+            @includeWhen($title === 'Keanggotaan', 'components.content.main')
+
+            {{-- Tulis kode di bawah ini untuk isi kontennya --}}
+            
+        </x-layout.content-bar>
+    </div>
+
+    {{-- SweetAlert2 --}}
+    @if (session('status'))
+        <script>
+            Swal.fire({
+                icon: 'success',
+                title: 'Sukses!',
+                text: '{{ session('status') }}',
+                timer: 2500,
+                showConfirmButton: false
+            });
+        </script>
+    @endif
+</body>
