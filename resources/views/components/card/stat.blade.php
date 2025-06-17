e9c2310b1c4565dc00309099adb2c43616d33cc6
atalamahardika <atalamahardika24@gmail.com>
2025-06-17 11:54:32 +0700

    Mengubah warna background menjadi hijau tua

--- resources/views/components/card/stat.blade.php
+++ resources/views/components/card/stat.blade.php
@@ -1,6 +1,6 @@
 @props(['title', 'count'])
 
-<div class="p-6 rounded-xl text-white shadow-md flex flex-col justify-center items-center" style="background: linear-gradient(to right, #79E133, #55C733);">
+<div class="p-6 rounded-xl text-white shadow-md flex flex-col justify-center items-center" style="background: linear-gradient(to right, #15400E, #006400);">
     <h3 class="text-xl font-semibold mb-2" style="color: white !important;">{{ $title }}</h3>
     <p class="text-3xl font-bold">{{ $count }}</p>
 </div>
